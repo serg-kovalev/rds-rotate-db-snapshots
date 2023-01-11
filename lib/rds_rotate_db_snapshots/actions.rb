076f300c07b1555e83e9fb434838e6e8e88a73c1
Siarhei Kavaliou <kovserg@gmail.com>
2023-01-11 13:50:56 +0300

    Refactor Actions

--- lib/rds_rotate_db_snapshots/actions.rb
+++ lib/rds_rotate_db_snapshots/actions.rb
@@ -1,49 +1,19 @@
 class RdsRotateDbSnapshots
   module Actions
     def rotate_em(snapshots)
-      # poor man's way to get a deep copy of our time_periods definition hash
-      periods = Marshal.load(Marshal.dump(time_periods))
-
       snapshots.each do |snapshot|
         time = snapshot[:snapshot_create_time]
-        db_id = snapshot[:db_instance_identifier]
         snapshot_id = snapshot[:db_snapshot_identifier]
         description = snapshot_id
-        keep_reason = nil
 
         if options[:pattern] && description !~ /#{options[:pattern]}/
           puts "  #{time.strftime '%Y-%m-%d %H:%M:%S'} #{snapshot_id} Skipping snapshot with description #{description}"
           next
         end
 
-        periods.keys.sort { |a, b| periods[a][:seconds] <=> periods[b][:seconds] }.each do |period|
-          period_info = periods[period]
-          keep = period_info[:keep]
-          keeping = period_info[:keeping]
-
-          time_string = time.strftime period_info[:format]
-          next unless Time.now - time < keep * period_info[:seconds]
-
-          if !keeping.key?(time_string) && keeping.length < keep
-            keep_reason = period
-            keeping[time_string] = snapshot
-          end
-          break
-        end
-
+        keep_reason = rotate_period_based_decision
         keep_reason = 'last snapshot' if keep_reason.nil? && snapshot == snapshots.last && options[:keep_last]
-
-        if keep_reason.nil?
-          puts "  #{time.strftime '%Y-%m-%d %H:%M:%S'} #{snapshot_id} Deleting"
-          begin
-            client.delete_db_snapshot(db_snapshot_identifier: snapshot_id) unless options[:dry_run]
-          rescue Aws::RDS::Errors => e
-            backoff
-            retry
-          end
-        else
-          puts "  #{time.strftime '%Y-%m-%d %H:%M:%S'} #{snapshot_id} Keeping for #{keep_reason}"
-        end
+        rotate_or_keep_snapshot(keep_reason, time, snapshot_id)
       end
     end
 
@@ -52,17 +22,7 @@ class RdsRotateDbSnapshots
 
       name = name.gsub(/[^a-zA-Z0-9-]/, '')
       if name.size.positive?
-        name = "#{name}-#{Time.now.strftime('%Y%m%d%H%M%S')}"
-        db_indentifier_ids.each do |db_id|
-          unless options[:dry_run]
-            client.create_db_snapshot(db_snapshot_identifier: name,
-                                      db_instance_identifier: db_id)
-          end
-          puts "  #{Time.now.strftime '%Y-%m-%d %H:%M:%S'} Creation snapshot #{name} is pending (db: #{db_id})"
-        rescue Aws::RDS::Errors::InvalidDBInstanceStateFault => e
-          backoff
-          retry
-        end
+        create_db_snapshots(name: name, db_indentifier_ids: db_indentifier_ids)
       else
         puts "invalid snapshot name format - #{name}"
         exit 1
@@ -99,6 +59,59 @@ class RdsRotateDbSnapshots
                   .sort { |a, b| a[:snapshot_create_time] <=> b[:snapshot_create_time] }
 
       rotate_em snapshots
+    end
+
+    private
+
+    def rotate_period_based_decision
+      # poor man's way to get a deep copy of our time_periods definition hash
+      periods = Marshal.load(Marshal.dump(time_periods))
+      keep_reason = nil
+
+      periods.keys.sort { |a, b| periods[a][:seconds] <=> periods[b][:seconds] }.each do |period|
+        period_info = periods[period]
+        keep = period_info[:keep]
+        keeping = period_info[:keeping]
+
+        time_string = time.strftime period_info[:format]
+        next unless Time.now - time < keep * period_info[:seconds]
+
+        if !keeping.key?(time_string) && keeping.length < keep
+          keep_reason = period
+          keeping[time_string] = snapshot
+        end
+        break
+      end
+
+      keep_reason
+    end
+
+    def rotate_or_keep_snapshot(keep_reason, time, snapshot_id)
+      if keep_reason.nil?
+        puts "  #{time.strftime '%Y-%m-%d %H:%M:%S'} #{snapshot_id} Deleting"
+        begin
+          client.delete_db_snapshot(db_snapshot_identifier: snapshot_id) unless options[:dry_run]
+        rescue Aws::RDS::Errors => e
+          backoff
+          retry
+        end
+      else
+        puts "  #{time.strftime '%Y-%m-%d %H:%M:%S'} #{snapshot_id} Keeping for #{keep_reason}"
+      end
+    end
+
+    def create_db_snapshots(name:, db_indentifier_ids:)
+      name = "#{name}-#{Time.now.strftime('%Y%m%d%H%M%S')}"
+      db_indentifier_ids.each do |db_id|
+        unless options[:dry_run]
+          client.create_db_snapshot(db_snapshot_identifier: name,
+                                    db_instance_identifier: db_id)
+        end
+        puts "  #{Time.now.strftime '%Y-%m-%d %H:%M:%S'} Creation snapshot #{name} is pending (db: #{db_id})"
+      rescue Aws::RDS::Errors::InvalidDBInstanceStateFault
+        backoff
+        retry
+      end
     end
   end
 end
